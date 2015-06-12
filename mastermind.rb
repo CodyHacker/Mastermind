f06814ab4d847b0e06cebb68f2d9feb9e2149336
Cody Hacker <jbalsamo@cox.net>
2015-06-12 14:13:21 -0700

    made @the_code an instance variable

--- mastermind.rb
+++ mastermind.rb
@@ -5,19 +5,19 @@ class CodeMaker
   def initialize(number_of_pins=4, number_of_colors=6)
     @number_of_pins = number_of_pins
     @number_of_colors = number_of_colors
+    @the_code = []
   end
 
   def make_code
-    the_code = []
     @number_of_pins.times do
-      the_code << random_color
+      @the_code << random_color
     end
-    the_code
+    @the_code
   end
 
-  def to_s
-    puts "Number of pins: #{@number_of_pins}, Number of colors possible per pin: #{@number_of_colors}"
-  end
+  # def to_s
+  #   puts "Number of pins: #{@number_of_pins}, Number of colors possible per pin: #{@number_of_colors}"
+  # end
 
 
 private
@@ -27,7 +27,9 @@ private
   
 end
 
-new_code_maker = CodeMaker.new
+code_maker1 = CodeMaker.new
+code_maker2 = CodeMaker.new
 
 
-p new_code_maker.make_code
+p code_maker1.make_code
+p code_maker2.make_code
